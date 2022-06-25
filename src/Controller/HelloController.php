0f0673e2629f1f0d83b1c74224ced3c834cd1888
Piotr Jura <jura.piotrek@gmail.com>
2022-06-25 15:25:48 +0200

    0014-routing-using-attributes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+  private array $messages = [
+    "Hello", "Hi", "Bye!"
+  ];
+
+  #[Route('/', name: 'app_index')]
   public function index(): Response
   {
-    return new Response("HI!");
+    return new Response(implode(',', $this->messages));
+  }
+
+  #[Route('/messages/{id}', name: 'app_show_one')]
+  public function showOne($id): Response
+  {
+    return new Response($this->messages[$id]);
   }
 }
