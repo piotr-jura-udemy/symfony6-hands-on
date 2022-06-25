d6681cc85450ad0548eddb1d59c290fccc9cb0ab
Piotr Jura <jura.piotrek@gmail.com>
2022-06-25 15:55:12 +0200

    0015-route-params-requirements-optional-params

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -11,14 +11,16 @@ class HelloController
     "Hello", "Hi", "Bye!"
   ];
 
-  #[Route('/', name: 'app_index')]
-  public function index(): Response
+  #[Route('/{limit?3}', name: 'app_index')]
+  public function index(int $limit): Response
   {
-    return new Response(implode(',', $this->messages));
+    return new Response(
+      implode(',', array_slice($this->messages, 0, $limit))
+    );
   }
 
-  #[Route('/messages/{id}', name: 'app_show_one')]
-  public function showOne($id): Response
+  #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+  public function showOne(int $id): Response
   {
     return new Response($this->messages[$id]);
   }
