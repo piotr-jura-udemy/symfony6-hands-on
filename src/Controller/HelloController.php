848a0a322ae3609ce372c11fda9cc0fdd8c667e7
Piotr Jura <jura.piotrek@gmail.com>
2022-06-25 17:51:43 +0200

    0016-twig-templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController
+class HelloController extends AbstractController
 {
   private array $messages = [
     "Hello", "Hi", "Bye!"
@@ -14,14 +15,23 @@ class HelloController
   #[Route('/{limit?3}', name: 'app_index')]
   public function index(int $limit): Response
   {
-    return new Response(
-      implode(',', array_slice($this->messages, 0, $limit))
+    return $this->render(
+      'hello/index.html.twig',
+      [
+        'message' => implode(',', array_slice($this->messages, 0, $limit))
+      ]
     );
   }
 
   #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
   public function showOne(int $id): Response
   {
-    return new Response($this->messages[$id]);
+    return $this->render(
+      'hello/show_one.html.twig',
+      [
+        'message' => $this->messages[$id]
+      ]
+    );
+    // return new Response($this->messages[$id]);
   }
 }
