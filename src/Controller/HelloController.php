413cd83c03b9b5e23d99e00dcec4a937d99e80a4
Piotr Jura <jura.piotrek@gmail.com>
2022-06-26 12:29:56 +0200

    0018-twig-control-structures

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -18,7 +18,7 @@ class HelloController extends AbstractController
     return $this->render(
       'hello/index.html.twig',
       [
-        'message' => implode(',', array_slice($this->messages, 0, $limit))
+        'messages' => array_slice($this->messages, 0, $limit)
       ]
     );
   }
