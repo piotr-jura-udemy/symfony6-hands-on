f9e30abab8ba54df9de6b31330bceb53ce81c761
Piotr Jura <jura.piotrek@gmail.com>
2022-06-26 19:46:49 +0200

    0052-running-mysql-and-connecting-from-symfony

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
