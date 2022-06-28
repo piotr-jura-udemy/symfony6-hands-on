fe99132bd9f57d1025ac827ce781d549557aec13
Piotr Jura <jura.piotrek@gmail.com>
2022-06-28 17:53:05 +0200

    0056-repository

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -14,7 +14,7 @@ class HelloController extends AbstractController
     ['message' => 'Bye!', 'created' => '2021/05/12']
   ];
 
-  #[Route('/{limit?3}', name: 'app_index')]
+  #[Route('/{limit<\d+>?3}', name: 'app_index')]
   public function index(int $limit): Response
   {
     return $this->render(
--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('It comes from controller');
+        // $microPost->setText('Hi!');
+        // $microPost->setCreated(new DateTime());
+
+        // $microPost = $posts->find(7);
+        // $posts->remove($microPost, true);
+
+        // dd($posts->findBy(['title' => 'Welcome to US!']));
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
