76947df0ce7c1d79e56f8b0bc3c001e98b15512f
Piotr Jura <jura.piotrek@gmail.com>
2022-06-29 19:06:27 +0200

    0081-handling-form-submission

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -29,7 +30,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
@@ -37,6 +38,17 @@ class MicroPostController extends AbstractController
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+
+            // Add a flash
+            // Redirect
+        }
 
         return $this->renderForm(
             'micro_post/add.html.twig',
