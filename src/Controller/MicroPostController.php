d59ee5272027009d29761ee626801ac72c7d45fd
Piotr Jura <jura.piotrek@gmail.com>
2022-06-30 19:42:02 +0200

    0085-edit-form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -59,4 +59,33 @@ class MicroPostController extends AbstractController
             ]
         );
     }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('text')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $posts->add($post, true);
+
+            // Add a flash
+            $this->addFlash('success', 'Your micro post have been updated.');
+
+            return $this->redirectToRoute('app_micro_post');
+            // Redirect
+        }
+
+        return $this->renderForm(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
 }
