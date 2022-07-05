de9f8432c7fc25c5938e407415c7f15396f8e6de
Piotr Jura <jura.piotrek@gmail.com>
2022-07-05 19:21:45 +0200

    0124-working-with-one-to-many

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\User;
 use App\Entity\Comment;
 use App\Entity\MicroPost;
 use App\Entity\UserProfile;
+use App\Controller\MicroPostController;
+use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use App\Repository\UserProfileRepository;
-use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,18 +24,19 @@ class HelloController extends AbstractController
   ];
 
   #[Route('/', name: 'app_index')]
-  public function index(MicroPostRepository $posts): Response
+  public function index(MicroPostRepository $posts, CommentRepository $comments): Response
   {
-    $post = new MicroPost();
-    $post->setTitle('Hello');
-    $post->setText('Hello');
-    $post->setCreated(new DateTime());
+    // $post = new MicroPost();
+    // $post->setTitle('Hello');
+    // $post->setText('Hello');
+    // $post->setCreated(new DateTime());
 
-    $comment = new Comment();
-    $comment->setText('Hello');
-    // $comment->setPost($post);
-    // $post->addComment($comment);
-    $posts->add($post, true);
+    // $post = $posts->find(19);
+    // $comment = $post->getComments()[0];
+    // $comment->setPost(null);
+    // $comments->add($comment, true);
+
+    // dd($post);
 
     // $user = new User();
     // $user->setEmail('email@email.com');
--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\MicroPostRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\MicroPostRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MicroPostRepository::class)]
