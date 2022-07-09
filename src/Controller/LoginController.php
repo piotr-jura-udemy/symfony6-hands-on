a01d01afa9d0847aa343f2fe1d3566cc36fa01de
Piotr Jura <jura.piotrek@gmail.com>
2022-07-09 20:13:32 +0200

    0183-auth-using-form

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(
+        AuthenticationUtils $utils
+    ): Response {
+        $lastUsername = $utils->getLastUsername();
+        $error = $utils->getLastAuthenticationError();
+
+        return $this->render('login/index.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
