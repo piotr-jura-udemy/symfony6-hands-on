c54cd582af7cd38b20b06866b746fde11406932e
Piotr Jura <jura.piotrek@gmail.com>
2022-07-10 15:14:47 +0200

    0187-getting-the-current-user

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -50,6 +50,7 @@ class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             $post->setCreated(new DateTime());
+            $post->setAuthor($this->getUser());
             $posts->add($post, true);
 
             // Add a flash
@@ -102,6 +103,7 @@ class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $comment = $form->getData();
             $comment->setPost($post);
+            $comment->setAuthor($this->getUser());
             $comments->add($comment, true);
 
             // Add a flash
