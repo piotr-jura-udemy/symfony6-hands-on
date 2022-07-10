11432f42ace047dfa094e544068502bec4775672
Piotr Jura <jura.piotrek@gmail.com>
2022-07-10 13:23:31 +0200

    0184-logging-out

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -21,4 +21,9 @@ class LoginController extends AbstractController
             'error' => $error
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
 }
