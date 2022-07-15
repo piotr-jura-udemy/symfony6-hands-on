5b6ed2122ed7c896e1b45a8c4f7454b0bf1d3bd9
Piotr Jura <jura.piotrek@gmail.com>
2022-07-15 16:55:20 +0200

    0211-embedding-forms

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function profile(
-        Request $request
+        Request $request,
+        UserRepository $users
     ): Response {
         /** @var User $user */
         $user = $this->getUser();
@@ -28,9 +32,16 @@ class SettingsProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userProfile = $form->getData();
-            // Save this somehow
-            // Add the flash message
-            // Redirect
+            $user->setUserProfile($userProfile);
+            $users->add($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile settings were saved.'
+            );
+
+            return $this->redirectToRoute(
+                'app_settings_profile'
+            );
         }
 
         return $this->render(
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,15 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -53,7 +54,8 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ]);
+            ])
+            ->add('userProfile', UserProfileType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
