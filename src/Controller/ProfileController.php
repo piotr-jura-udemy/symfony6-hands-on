ff82adfba32b0310c4af8671bc6b55cdaf06e115
Piotr Jura <jura.piotrek@gmail.com>
2022-07-17 16:42:46 +0200

    0217-profile-page-tabs

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -16,4 +16,20 @@ class ProfileController extends AbstractController
             'user' => $user
         ]);
     }
+
+    #[Route('/profile/{id}/follows', name: 'app_profile_follows')]
+    public function follows(User $user): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/profile/{id}/followers', name: 'app_profile_followers')]
+    public function followers(User $user): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'user' => $user
+        ]);
+    }
 }
