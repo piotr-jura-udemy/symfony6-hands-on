6382a50135d577e2bf185d70123ce99f967f81a7
Piotr Jura <jura.piotrek@gmail.com>
2022-07-18 10:38:51 +0200

    0230-reusable-queries

--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\MicroPost;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<MicroPost>
@@ -41,12 +42,40 @@ class MicroPostRepository extends ServiceEntityRepository
 
     public function findAllWithComments(): array
     {
-        return $this->createQueryBuilder('p')
-            ->addSelect('c')
-            ->leftJoin('p.comments', 'c')
-            ->orderBy('p.created', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return $this->findAllQuery(
+            withComments: true
+        )->getQuery()->getResult();
+    }
+
+    private function findAllQuery(
+        bool $withComments = false,
+        bool $withLikes = false,
+        bool $withAuthors = false,
+        bool $withProfiles = false,
+    ): QueryBuilder {
+        $query = $this->createQueryBuilder('p');
+
+        if ($withComments) {
+            $query->leftJoin('p.comments', 'c')
+                ->addSelect('c');
+        }
+
+        if ($withLikes) {
+            $query->leftJoin('p.likedBy', 'l')
+                ->addSelect('l');
+        }
+
+        if ($withAuthors || $withProfiles) {
+            $query->leftJoin('p.author', 'a')
+                ->addSelect('a');
+        }
+
+        if ($withProfiles) {
+            $query->leftJoin('a.userProfile', 'up')
+                ->addSelect('up');
+        }
+
+        return $query->orderBy('p.created', 'DESC');
     }
 
     //    /**
