4d24b82b8cde7669eaaace8419250daa5f887f39
Piotr Jura <jura.piotrek@gmail.com>
2022-07-19 18:21:50 +0200

    0234-posts-from-people-you-follow

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Comment;
 use App\Entity\MicroPost;
 use App\Form\CommentType;
@@ -39,12 +40,18 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/follows', name: 'app_micro_post_follows')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function follows(MicroPostRepository $posts): Response
     {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
         return $this->render(
             'micro_post/follows.html.twig',
             [
-                'posts' => $posts->findAllWithComments(),
+                'posts' => $posts->findAllByAuthors(
+                    $currentUser->getFollows()
+                ),
             ]
         );
     }
--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -3,11 +3,12 @@
 namespace App\Repository;
 
 use App\Entity\User;
+use Doctrine\ORM\Query;
 use App\Entity\MicroPost;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Query;
 
 /**
  * @extends ServiceEntityRepository<MicroPost>
@@ -61,6 +62,22 @@ class MicroPostRepository extends ServiceEntityRepository
             ->setParameter(
                 'author',
                 $author instanceof User ? $author->getId() : $author
+            )->getQuery()
+            ->getResult();
+    }
+
+    public function findAllByAuthors(
+        Collection|array $authors
+    ): array {
+        return $this->findAllQuery(
+            withComments: true,
+            withLikes: true,
+            withAuthors: true,
+            withProfiles: true
+        )->where('p.author IN (:authors)')
+            ->setParameter(
+                'authors',
+                $authors
             )->getQuery()
             ->getResult();
     }
